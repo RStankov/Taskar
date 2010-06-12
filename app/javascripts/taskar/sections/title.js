35d8ead86eb437b4a2d10907e90d2db89c9df67d
RStankov <rstankov@gmail.com>
2010-06-12 14:44:42 +0300

    finish the edit section

--- app/javascripts/taskar/sections/title.js
+++ app/javascripts/taskar/sections/title.js
@@ -4,6 +4,7 @@ Taskar.Sections.Title = function(section){
   
    head.down('a.edit').observe('click', toggle);
    form.down('input[type=button]').observe('click', toggle);
+   form.observe('submit', validate);
    form.observe('keyup', function(e){
      if (e.keyCode == Event.KEY_ESC){
        toggle(e);
@@ -14,6 +15,17 @@ Taskar.Sections.Title = function(section){
      if (e && 'stop' in e) e.stop();
      
      head.toggle();
+     form.reset();
      form.toggle();
+   }
+   
+   var highlight = new S2.FX.Highlight(section, {after: function(e){ e.element.style.backgroundColor = null; }});
+   function validate(e){
+     var input = e.findElement('form').down('input[type=text]');
+     if (input.getValue().trim().length == 0){
+       e.stop();
+       Taskar.FX.shake(input, {distance: 2, turns: 2});
+       highlight.play(input);
+     }
    }
 }
