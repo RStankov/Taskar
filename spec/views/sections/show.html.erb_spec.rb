92338758439ee5149dc76bdf5f26188da505a8e8
RStankov <rstankov@gmail.com>
2010-06-12 23:54:30 +0300

    use factory insted of mocks here

--- spec/views/sections/show.html.erb_spec.rb
+++ spec/views/sections/show.html.erb_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe "/sections/show.html.erb" do
   before(:each) do
-    assigns[:project] = @project = stub_model(Project)
-    assigns[:section] = @section = stub_model(Section,
-      :project => 1,
-      :name => "value for name",
-      :position => 1
-    )
+    assigns[:section] = @section = Factory(:section)
+    assigns[:project] = @project = @section.project
   end
 
   it "should renders" do
