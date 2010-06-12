e608f72f70eb5731c7571faafc347570d1df1c10
RStankov <rstankov@gmail.com>
2010-06-12 03:08:56 +0300

    add little effect to new section form

--- app/javascripts/taskar/new_section_form.js
+++ app/javascripts/taskar/new_section_form.js
@@ -4,19 +4,30 @@ Taskar.NewSectionForm = function(sectionsBar){
   sectionsBar.on('keyup',  'form',               hideOnEsc);
   sectionsBar.on('submit', 'form',               validate);
   
+  var appear = new S2.FX.Style(sectionsBar, {
+    before: function(e){ e.element.setStyle({ width: '0px', opacity: 0.0 }); },
+    after:  function(e){ e.element.down('input[type=text]').focus(); }
+  });
+  
   function show(e, element){
     e.stop();
     
     var form = $('new_section');
     form.down('form').reset();
   
+    if (element.previous() == form){
+      return;
+    }
+    
+    form.hide();
+  
     element.hide()
     element.show.bind(element).defer();
-    
-    element.insert({before: form.show()});
+    element.insert({before: form});
     
     form.show();
-    form.down('input[type=text]').focus();
+    
+    appear.play(form, {style: 'opacity:1; width:' + form.getWidth() + 'px'})
   }
   
   function hide(e, element){
