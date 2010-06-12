49b185172e993587ca6048350b0543c9ffb54c75
RStankov <rstankov@gmail.com>
2010-06-12 14:48:03 +0300

    reformat js

--- app/javascripts/taskar/sections/title.js
+++ app/javascripts/taskar/sections/title.js
@@ -6,30 +6,30 @@ Taskar.Sections.Title = function(section){
     return;
   }
   
-   head.down('a.edit').observe('click', toggle);
-   form.down('input[type=button]').observe('click', toggle);
-   form.observe('submit', validate);
-   form.observe('keyup', function(e){
-     if (e.keyCode == Event.KEY_ESC){
-       toggle(e);
-     }
-   });
+  head.down('a.edit').observe('click', toggle);
+  form.down('input[type=button]').observe('click', toggle);
+  form.observe('submit', validate);
+  form.observe('keyup', function(e){
+    if (e.keyCode == Event.KEY_ESC){
+      toggle(e);
+    }
+  });
    
-   function toggle(e){
-     if (e && 'stop' in e) e.stop();
+  function toggle(e){
+    if (e && 'stop' in e) e.stop();
      
-     head.toggle();
-     form.reset();
-     form.toggle();
-   }
+    head.toggle();
+    form.reset();
+    form.toggle();
+  }
    
-   var highlight = new S2.FX.Highlight(section, {after: function(e){ e.element.style.backgroundColor = null; }});
-   function validate(e){
-     var input = e.findElement('form').down('input[type=text]');
-     if (input.getValue().trim().length == 0){
-       e.stop();
-       Taskar.FX.shake(input, {distance: 2, turns: 2});
-       highlight.play(input);
-     }
-   }
+  var highlight = new S2.FX.Highlight(section, {after: function(e){ e.element.style.backgroundColor = null; }});
+  function validate(e){
+    var input = e.findElement('form').down('input[type=text]');
+    if (input.getValue().trim().length == 0){
+      e.stop();
+      Taskar.FX.shake(input, {distance: 2, turns: 2});
+      highlight.play(input);
+    }
+  }
 }
