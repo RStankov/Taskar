ec8c77ecc01452ec08749cb06a3c78439a756def
RStankov <rstankov@gmail.com>
2010-06-12 22:41:22 +0300

    add insert after to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,20 @@ class Task < ActiveRecord::Base
   validates_presence_of :section
   validates_inclusion_of :status, :in => [-1, 0, 0]
   
-  attr_accessible :text
+  attr_accessor :insert_before
   
+  attr_accessible :text, :insert_before
+  
+  acts_as_list :scope => :section
+  
+  default_scope :order => "position ASC"
+  
+  def add_to_list_bottom
+    self[position_column] = if insert_before && record = Task.find(:first, :conditions => {:id => insert_before})
+      increment_positions_on_lower_items record.position
+      record.position
+    else
+      bottom_position_in_list.to_i + 1
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Task do
   it { should allow_mass_assignment_of(:text) }
+  it { should allow_mass_assignment_of(:insert_before) }
   it { should_not allow_mass_assignment_of(:status) }
   it { should_not allow_mass_assignment_of(:position) }
   it { should_not allow_mass_assignment_of(:section_id) }
@@ -11,4 +12,61 @@ describe Task do
   it { should validate_presence_of(:text) }
   it { should validate_presence_of(:section) }
   
+  describe "acts_as_list" do
+    before do
+      @section = Factory(:section)
+    end
+    
+    def create_next_task
+      Factory(:task, :section_id => @section.id)
+    end
+    
+    def should_have_order_of(*tasks)
+      tasks = tasks.map(&:reload)
+      tasks.collect(&:id).should == tasks.sort_by(&:position).collect(&:id)
+    end
+    
+    it "should be scoped at section" do
+      task_0 = Factory(:task)
+      
+      should_have_order_of(create_next_task, create_next_task, create_next_task)
+      
+      task_0.position.should == 1
+    end
+    
+    describe "insert_before property" do
+      before do
+        @task_1 = create_next_task
+        @task_2 = create_next_task
+        @task_3 = create_next_task
+      end
+      
+      it "should be inserted before 1" do
+        @task = Factory(:task, :section_id => @section.id, :insert_before => @task_1.id)
+        
+        should_have_order_of(@task, @task_1, @task_2, @task_3)
+      end
+      
+      
+      it "should be inserted before 2" do
+        @task = Factory(:task, :section_id => @section.id, :insert_before => @task_2.id)
+        
+        should_have_order_of(@task_1, @task, @task_2, @task_3)
+      end
+      
+      
+      it "should be inserted before 3" do
+        @task = Factory(:task, :section_id => @section.id, :insert_before => @task_3.id)
+        
+        should_have_order_of(@task_1, @task_2, @task, @task_3)
+      end
+
+      it "should be inserted last" do
+        @task = Factory(:task, :section_id => @section.id, :insert_before => nil)
+
+        should_have_order_of(@task_1, @task_2, @task_3, @task)
+      end
+      
+    end
+  end
 end
