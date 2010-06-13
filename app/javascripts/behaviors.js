94bae3032d33cc4948348f2d377ee84ac1bea864
RStankov <rstankov@gmail.com>
2010-06-13 15:56:05 +0300

    hide new task form and show it when needed

--- app/javascripts/behaviors.js
+++ app/javascripts/behaviors.js
@@ -1,4 +1,23 @@
 CD3.Behaviors({
   '#nav_sections':  Taskar.Sections.NewForm,
-  '#section_title': Taskar.Sections.Title
+  '#section_title': Taskar.Sections.Title,
+  '#tasks:click': {
+    '.cancel': function(e, element){
+      e.findElement('li').slideUp();
+    }
+  },
+  '#add_task_button:click': function(){
+    var newTask = $('new_task');
+        
+    newTask.down('form').reset();
+    
+    if (newTask.visible() && !newTask.next()){
+      return newTask.down('textarea').focus();
+    }
+    
+    newTask.slideDown(function(e){
+      e.element.down('textarea').focus();
+      new Taskar.FX.ScrollTo(e.element);
+    });
+  }
 })
