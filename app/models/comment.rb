c221e942b9fc87fcad45947f39fc0cc784fc5c0b
RStankov <rstankov@gmail.com>
2010-06-18 01:01:32 +0300

    added Comment#editable_by

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,10 @@ class Comment < ActiveRecord::Base
   validates_presence_of :text
   
   attr_accessible :text
+  
+  EDITABLE_BY = 15.minutes
+  
+  def editable_by user
+    self.user_id == user.id && updated_at + EDITABLE_BY > Time.now
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,4 +9,24 @@ describe Comment do
   
   it_should_have_counter_cache_of :task
   it_should_have_counter_cache_of :user
+  
+  describe "editable_by" do
+    before do
+      @comment = Factory(:comment)
+    end
+    
+    it "should be false for user who is not comment creator" do
+      @comment.editable_by(Factory(:user)).should be_false
+    end
+    
+    it "should be true for the current user (if updated_at in Comment.EDITABLE_BY time)" do
+      @comment.updated_at = Time.now
+      @comment.editable_by(@comment.user).should be_true
+    end
+    
+    it "should be false for the current user (if updated_at out of Comment.EDITABLE_BY time)" do
+      @comment.updated_at = Time.now - 2 * Comment::EDITABLE_BY
+      @comment.editable_by(@comment.user).should be_false
+    end
+  end
 end
