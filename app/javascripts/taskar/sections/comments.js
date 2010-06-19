e6302916ac0900c7496ea2e5dfeee205cdaff0e7
RStankov <rstankov@gmail.com>
2010-06-19 17:47:22 +0300

    use data-remote for deleting comments

--- app/javascripts/taskar/sections/comments.js
+++ app/javascripts/taskar/sections/comments.js
@@ -1,13 +1,7 @@
 Taskar.Sections.Comments = {
-  click: {
-    '.delete': function(e, element){
-      e.stop();
-      element.request({
-        method: 'delete',
-        onCreate: function(){
-          element.up('.comment').removeWithEffect('slideUp');
-        }
-      });
+  'ajax:delete': {
+    '.comment': function(e, element){
+      element.removeWithEffect('slideUp');
     }
   }
 };
