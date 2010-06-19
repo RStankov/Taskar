78e09a57d042bd5bc06f40fb3d198da8fce740fb
RStankov <rstankov@gmail.com>
2010-06-19 14:06:41 +0300

    move task title to its own partial

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,6 +5,6 @@ module TasksHelper
   end
   
   def task_description(task)
-    '<p>' + t(:'.description', :from => 'Радослав Станков', :to => 'Някой друг', :on => '10.05.2010', :due => '10.05.2010') + '</p>'
+    '<p>' + t(:'tasks.show.description', :from => 'Радослав Станков', :to => 'Някой друг', :on => '10.05.2010', :due => '10.05.2010') + '</p>'
   end
 end
