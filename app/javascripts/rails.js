a0bb1ae855a21bd80cd658c4fbf50a7190d34ef4
RStankov <rstankov@gmail.com>
2010-06-22 22:30:29 +0300

    check data method not only for delete requests

--- app/javascripts/rails.js
+++ app/javascripts/rails.js
@@ -1,13 +1,18 @@
 (function(){  
-  document.on('click', 'a[data-method=delete]', function(e, element){
+  document.on('click', 'a[data-method]', function(e, element){
   	if (element.hasAttribute('data-remote')) return;
 
   	e.stop();
 
   	if (!element.hasAttribute('data-confirm') || confirm(element.getAttribute('data-confirm'))){
-  		var form = Element('form', {method: 'post', action: element.href}).hide()
-  			.insert(Element('input', {type: 'hidden', name: '_method', value: 'delete'}))
-  			.insert(Element('input', {type: 'hidden', name: 'authenticity_token', value: element.getAttribute('data-token')}));
+  		var method  = element.getAttribute('data-method');
+  		    form    = Element('form', {method: 'post', action: element.href}).hide();
+  		
+  		form.insert(Element('input', {type: 'hidden', name: 'authenticity_token', value: element.getAttribute('data-token')}));
+
+      if (method != 'post'){
+        form.insert(Element('input', {type: 'hidden', name: '_method', value: method}))
+      }
 
   		element.insert({after: form});
 
