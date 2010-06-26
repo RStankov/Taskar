71dbce04c9e97ab9c23e52e82f78fa609a0b19c4
RStankov <rstankov@gmail.com>
2010-06-26 20:37:16 +0300

    add draft for ordering sections via dnd

--- app/javascripts/behaviors.js
+++ app/javascripts/behaviors.js
@@ -17,3 +17,31 @@ CD3.Behaviors({
     }
   }
 });
+
+/*
+CD3.Behaviors('#nav_sections', function(element){
+  new Taskar.Dnd.Sortable(element.parentNode, {
+    item:   '.section',
+    handle: false,
+    moveX:  true,
+    moveY:  false
+  });
+  
+  var displayLinks = new Event.Handler(element, 'click', 'a', Event.stop);
+  displayLinks.stop = displayLinks.stop.bind(displayLinks);
+  
+  element.select('.add_section').each(function(add){
+    (add.next('.section') || element).store('add_section', add);
+  });
+  
+  element.observe('drag:start', function(){ displayLinks.start() });
+  element.observe('drag:finish', function(){
+    displayLinks.stop.defer();
+    
+    element.insert({bottom: element.retrieve('add_section')});
+    element.select('.section').each(function(section){
+      section.insert({ before: section.retrieve('add_section') });
+    });
+  });
+});
+*/
