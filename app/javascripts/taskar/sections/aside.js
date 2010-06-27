4fbbad75439fc6d1465c4dc610ec6b0d8397ae9f
RStankov <rstankov@gmail.com>
2010-06-28 01:33:07 +0300

    don't change the responsibilies count if it hasn't been changed

--- app/javascripts/taskar/sections/aside.js
+++ app/javascripts/taskar/sections/aside.js
@@ -2,11 +2,14 @@ Taskar.Sections.Aside = function(aside){
   var url = aside.getAttribute('data-update-path');
   
   function updateAside(t){
-    var data = t.responseJSON || {};
-    
-    $('user_card').down('span').morph('opacity:0', function(e){
-      e.element.update(data.responsibilities_count || 0).morph('opacity:1');
-    })
+    var data  = t.responseJSON || {},
+        count = $('user_card').down('span');
+        
+    if (count.innerHTML != data.responsibilities_count){
+      count.morph('opacity:0', function(e){
+        count.update(data.responsibilities_count || '').morph('opacity:1');
+      });
+    }
   }
   
   Ajax.Responders.register({
