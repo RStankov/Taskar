27c3dc5c91f5bcf2a791543d35d92a34fd55e771
RStankov <rstankov@gmail.com>
2010-06-27 03:42:50 +0300

    better formating for behaviors

--- app/javascripts/behaviors.js
+++ app/javascripts/behaviors.js
@@ -1,22 +1,17 @@
 CD3.Behaviors({
-  '#nav_sections':    function(){
-                        Taskar.Sections.NewForm(this);
-                        Taskar.Sections.Ordering(this);
-                      },
-  '#section_title':   Taskar.Sections.Title,
-  '#sections_show':   Taskar.Sections.Actions,
-  '#tasks':           Taskar.Sections.initTaskList,
-  '#comments':        Taskar.Sections.Comments,
-  '#live_search':     Taskar.UI.LiveSearch.Form,
+  '#nav_sections':        function(){
+                            Taskar.Sections.NewForm(this);
+                            Taskar.Sections.Ordering(this);
+                          },
+  '#section_title':       Taskar.Sections.Title,
+  '#sections_show':       Taskar.Sections.Actions,
+  '#tasks':               Taskar.Sections.initTaskList,
+  '#comments':            Taskar.Sections.Comments,
+  '#live_search':         Taskar.UI.LiveSearch.Form,
   '#scroll_to_top:click': function(e){
-    e.stop();
-    var id      = this.getAttribute('href').split('#').last(),
-        element = $(id);
-        
-    if (element){
-      new Taskar.FX.ScrollTo(element, function(){
-        location.hash = id;
-      });
-    }
-  }
+                            e.stop();
+                            var element, id = this.getAttribute('href').split('#').last();
+
+                            if (element = $(id)) new Taskar.FX.ScrollTo(element, function(){ location.hash = id; });
+                          }
 });
