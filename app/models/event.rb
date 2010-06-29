089fc881e63acdc28a07e4166d4ac62befc76b39
RStankov <rstankov@gmail.com>
2010-06-29 21:17:01 +0300

    when task is save take info from subject

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,24 @@ class Event < ActiveRecord::Base
   attr_readonly :project_id, :user_id
   attr_accessible :action, :subject
   
+  default_scope :order => 'updated_at DESC'
+  
   validates_presence_of :user, :project, :action, :subject_id, :subject_type
   
   before_validation_on_create :inherit_subject_project
   
-  default_scope :order => 'updated_at DESC'
+  before_save :set_info
   
   protected
     def inherit_subject_project
       self.project_id = subject.try(:project_id)
+    end
+    
+    def set_info
+      self.info = case subject_type
+        when "Section"  then subject.name
+        when "Task"     then subject.text
+        when "Comment"  then subject.task.text
+      end
     end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -20,4 +20,29 @@ describe Event do
     event.project_id.should  == section.project_id
   end
   
+  describe "info" do    
+    it "should be subject.text if subject is Task" do
+      task  = Factory(:task, :text => "task text")
+      event = Factory(:event, :subject => task)
+      
+      event.info.should == "task text"
+    end
+    
+    it "should be subject.task.text if subject is Comment" do
+      task    = Factory(:task, :text => "comment task text")
+      comment = Factory(:comment, :task => task)
+      event   = Factory(:event, :subject => comment)
+      
+      event.info.should == "comment task text"
+    end
+  
+  
+    it "should be subject.name if subject is Section" do
+      section = Factory(:section, :name => "section name")
+      event   = Factory(:event, :subject => section)
+      
+      event.info.should == "section name"
+    end
+  end
+  
 end
