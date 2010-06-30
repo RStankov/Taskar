7bfeddd2ff28b451e8032a5d8d7848c6026ec935
RStankov <rstankov@gmail.com>
2010-06-30 20:24:43 +0300

    section is no longer event subject

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -1,6 +1,6 @@
 Factory.define :event do |event|
   event.user    { |a| a.association :user }
-  event.subject { |a| a.association :section }
+  event.subject { |a| a.association :task }
   event.action  'created'
   event.info    'Section "Foo" created'
 end
