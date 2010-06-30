157d5b7f31a52b2890a811a3b1e78a56fc7edab7
RStankov <rstankov@gmail.com>
2010-06-30 23:50:51 +0300

    archived sections have different look

--- spec/views/sections/show.html.erb_spec.rb
+++ spec/views/sections/show.html.erb_spec.rb
@@ -7,13 +7,29 @@ describe "/sections/show.html.erb" do
     assigns[:section] = @section = Factory(:section)
     assigns[:project] = @project = @section.project
     
-    tasks = [Factory(:task)]
-    
-    @section.should_receive(:tasks).and_return(tasks)
-    tasks.should_receive(:unarchived).and_return(tasks)
+    @tasks = [Factory(:task)]
   end
 
-  it "should renders" do
+  it "should render active tasks"  do
+    @section.stub!(:archive?).and_return(false)    
+    @section.should_receive(:tasks).and_return(@tasks)
+    @tasks.should_receive(:unarchived).and_return(@tasks)
+    
     render
+    
+    response.should have_tag('#tasks')
+    response.should have_tag('#section_footer')
+  end
+  
+  it "should render archived tasks and no forms" do
+    @section.stub!(:archived?).and_return(true)
+    @section.should_receive(:tasks).and_return(@tasks)
+    @tasks.should_receive(:archived).and_return(@tasks)
+    
+    render
+    
+    response.should_not have_tag('#tasks')
+    response.should_not have_tag('#section_footer')
+    response.should have_tag('.tasks_list')
   end
 end
