6349b02dd8b3a09c18825e80d6e7555263ccadf7
RStankov <rstankov@gmail.com>
2010-07-01 00:03:45 +0300

    show/hide archive button

--- app/javascripts/taskar/sections/init_task_list.js
+++ app/javascripts/taskar/sections/init_task_list.js
@@ -3,6 +3,16 @@ Taskar.Sections.initTaskList = function(element){
     $('new_task').show().down('textarea').highlight().focus();
   }
   
+  if (element){
+    $('section_title').observe('mouseenter', checkForArchiveButton);
+    
+    function checkForArchiveButton(){
+      $('section_title').down('a.archive')[element.down('li.task') ? 'hide' : 'show']();
+    }
+    
+    checkForArchiveButton();
+  }
+  
   new Taskar.Dnd.Sortable(element, {
     list:   '#' + element.id,
     item:   'li.task',
