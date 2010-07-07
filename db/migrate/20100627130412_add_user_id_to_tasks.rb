d2ed8e4ebf4ccba60c85a250c4471a8d6fbb00f1
RStankov <rstankov@gmail.com>
2010-07-07 03:26:36 +0300

    check if user exists before migrating

--- db/migrate/20100627130412_add_user_id_to_tasks.rb
+++ db/migrate/20100627130412_add_user_id_to_tasks.rb
@@ -2,7 +2,7 @@ class AddUserIdToTasks < ActiveRecord::Migration
   def self.up
     add_column :tasks, :user_id, :integer
     
-    Task.update_all(:user_id => User.first.id)
+    Task.update_all(:user_id => User.first.id) if User.first
   end
 
   def self.down
