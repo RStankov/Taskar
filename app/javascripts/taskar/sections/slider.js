f0eb1e1605c1a48ba05262c9fe949c7aea9653f4
RStankov <rstankov@gmail.com>
2010-07-10 02:24:36 +0300

    adjusts sections slider values

--- app/javascripts/taskar/sections/slider.js
+++ app/javascripts/taskar/sections/slider.js
@@ -1,20 +1,19 @@
 Taskar.Sections.Slider = function(element){
-  var width       = element.select('li').invoke('getWidth').reduce(function(s, v){return s+=v}),
+  var width       = element.select('li.section').invoke('getWidth').reduce(function(s, v){ return s += v }),
       parent      = element.up(),
       maxWidth    = parent.getWidth(),
       leftRange   = 300,
       rightRange  = maxWidth - leftRange,
-      minSlide    = maxWidth * 2 - width;
+      minSlide    = maxWidth - width;
   
   if (width <= maxWidth){
     return;
   }
   
   element.style.width = width + 'px';
-  console.log(maxWidth - width, maxWidth, width);
+  
   var slide = new PeriodicalExecuter(function(timer){
     var position = (parseInt(element.style.marginLeft, 10) || 0) - timer.speed * 10;
-
     element.style.marginLeft = position.constrain(minSlide, 0) + 'px';
   }, 0.05);
   slide.start = function(speed){
