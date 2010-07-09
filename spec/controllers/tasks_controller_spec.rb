8f3b04b1532e6af2dee1e085f274743aefb8afd4
RStankov <rstankov@gmail.com>
2010-07-09 22:06:14 +0300

    better ident to tasks controller

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe TasksController do
   describe "with project user" do
     before { sign_with_project_user }
-    
+
     def controller_should_fire_event
       controller.should_receive(:activity).with(mock_task)
     end
@@ -13,15 +13,15 @@ describe TasksController do
         Task.stub(:find).with("37").and_return(mock_task)
         get :show, :id => "37"
       end
-    
+
       it "assigns the requested task as @task" do
         assigns[:task].should == mock_task
       end
-    
+
       it "assigns task's section as @section" do
         assigns[:section].should == mock_section
       end
-    
+
       it "assigns taks's section project as @project" do
         assigns[:project].should == mock_project
       end
@@ -41,36 +41,35 @@ describe TasksController do
       before do
         Section.stub(:find).with("2").and_return(mock_section)
       end
-    
+
       describe "with valid params" do
         before do
           @current_user.should_receive(:new_task).with(mock_section, {'these' => 'params'}).and_return(mock_task(:save => true))
-          
+
           controller_should_fire_event
         end
-      
+
         def params
           {:task => {:these => 'params'}, :section_id => "2"}
         end
-      
+
         it "assigns a newly created task as @task" do
           post :create, params
-        
+
           assigns[:task].should equal(mock_task)
         end
 
         it "redirects to the created task" do
           post :create, params
-        
+
           response.should redirect_to(section_url(mock_section, :anchor => "task_#{mock_task.id}"))
         end
-      
+
         it "renders create.rjs on xhr request" do
           xhr :post, :create, params
-        
+
           response.should render_template("create")
         end
-      
       end
 
       describe "with invalid params" do
@@ -78,7 +77,7 @@ describe TasksController do
           @current_user.should_receive(:new_task).with(mock_section, {'these' => 'params'}).and_return(mock_task(:save => false))
           post :create, :task => {:these => 'params'}, :section_id => "2"
         end
-        
+
         it "assigns a newly created but unsaved task as @task" do
           assigns[:task].should equal(mock_task)
         end
@@ -95,18 +94,18 @@ describe TasksController do
         Task.should_receive(:find).with("1").and_return(mock_task(:editable? => true))
         controller.should_receive(:ensure_task_is_editable)
       end
-    
+
       def params
         {:id => "1", :task => {:these => 'params'}}
       end
-    
+
       describe "with valid params" do
         before do
           mock_task.should_receive(:update_attributes).with("these" => "params").and_return(true)
-          
+
           controller_should_fire_event
         end
-      
+
         it "updates the requested task" do
           put :update, params
         end
@@ -120,7 +119,7 @@ describe TasksController do
           xhr :put, :update, params
           response.should render_template("show")
         end
-      
+
         it "redirects to tasks_url if not xhr" do
           put :update, params
           response.should redirect_to(task_url(mock_task))        
@@ -131,7 +130,7 @@ describe TasksController do
         before do
           mock_task.should_receive(:update_attributes).with("these" => "params").and_return(false)
         end
-      
+
         it "updates the requested task" do
           put :update, params
         end
@@ -146,22 +145,21 @@ describe TasksController do
           response.should render_template('edit')
         end
       end
-
     end
 
     describe "DELETE destroy" do
       before do
         Task.should_receive(:find).with("37").and_return(mock_task)
-        
+
         controller_should_fire_event
-        
+
         mock_task.should_receive(:destroy)
       end
-    
+
       def redirect_to_section_url
         redirect_to(section_url(mock_section))
       end
-    
+
       it "destroys the requested task" do
         delete :destroy, :id => "37"
       end
@@ -170,32 +168,32 @@ describe TasksController do
         delete :destroy, :id => "37"
         response.should redirect_to_section_url
       end
-    
+
       it "returns ok when this is xhr request" do
         xhr :delete, :destroy, :id => "37"
-      
+
         response.should be_success
         response.should_not redirect_to_section_url
       end
     end
-  
+
     describe "PUT state" do
       before do
         Task.stub!(:find).with("5").and_return(mock_task(:save => true))
         mock_task.stub!(:state=)
-        
+
         controller_should_fire_event
       end
-    
+
       it "updates state attribute" do
         mock_task.should_receive(:state=).with("fooo")
-      
+
         xhr :put, :state, :id => "5", :state => "fooo"
       end
-    
-      it "returns ok" do      
+
+      it "returns ok" do
         xhr :put, :state, :id => "5"
-      
+
         response.should be_success
       end
     end
@@ -205,16 +203,16 @@ describe TasksController do
         Project.should_receive(:find).with("3").and_return(mock_project)
         mock_project.should_receive(:tasks).and_return(Task)
       end
-      
+
       it "should reorder the given ids" do
         Task.should_receive(:reorder).with(["1", "2", "3", "4"])
-      
+
         xhr :put, :reorder, :project_id => "3", :items => ["1", "2", "3", "4"]
       end
-    
+
       it "should not render template" do
         xhr :put, :reorder, :project_id => "3"
-      
+
         response.should_not render_template(:reorder)
         response.should be_success
       end
@@ -223,22 +221,23 @@ describe TasksController do
     describe "GET search" do
       before do
         @tasks = [mock_task]
-      
+
         Project.should_receive(:find).with("3").and_return(mock_project)
         mock_project.should_receive(:tasks).and_return(@tasks)
+
         @tasks.should_receive(:unarchived).and_return(@tasks)
         @tasks.should_receive(:search).with("term").and_return(@tasks)
-      
+
         get :search, :project_id => "3", :ss => "term"
       end
-    
+
       it "should search for given :ss" do
       end
-    
+
       it "should assign founded tasks as @tasks" do
         assigns[:tasks].should == @tasks
       end
-    
+
       it "should render search template" do
         response.should render_template(:search)
       end
@@ -248,25 +247,25 @@ describe TasksController do
       before do
         Task.stub!(:find).with("5").and_return(mock_task(:save => true))
         mock_task.stub!(:archived=)
-        
+
         controller_should_fire_event
       end
-    
+
       it "should try to archive the task to true, when archived is true" do
         mock_task.should_receive(:archived=).with(true)
-      
+
         xhr :put, :archive, :id => "5", :archived => "true"
       end
-    
+
       it "should try to archive the task to false, when archived is set to false" do
         mock_task.should_receive(:archived=).with(false)
-      
+
         xhr :put, :archive, :id => "5"
       end
-    
+
       it "should just head ok" do
         xhr :put, :archive, :id => "5", :archived => "true"
-      
+
         response.should be_success
       end
     end
@@ -274,42 +273,42 @@ describe TasksController do
     describe "GET archived" do
       before do
         @tasks = [mock_task]
-      
+
         Section.should_receive(:find).with("1").and_return(mock_section)
         mock_section.should_receive(:tasks).and_return(@tasks)
         @tasks.should_receive(:archived).and_return(@tasks)
-      
+
         xhr :get, :archived, :section_id => "1"
       end
-    
+
       it "should assign the selected section as @section" do
         assigns[:section] = mock_section
       end
-    
+
       it "should get section archived items, and assign them as @tasks" do
         assigns[:tasks] = @tasks
       end
-    
+
       it "should render archived template" do
         response.should render_template(:archived)
       end
     end
-  
+
     describe "ensure_task_is_editable filter" do
       before do
         Task.stub(:find).with("1").and_return(mock_task)
         mock_task.should_receive(:editable?).and_return(false)
       end
-      
+
       it "should render :show action on xhr request" do
         xhr :get, :edit, :id => "1"
-        
+
         response.should render_template(:show)
       end
-      
+
       it "should redirect to @task on normal request" do
         get :edit, :id => "1"
-        
+
         response.should redirect_to(task_url(mock_task))
       end
     end
@@ -317,7 +316,7 @@ describe TasksController do
 
   describe "with user outside project" do
     before { sign_with_user_outside_the_project }
-    
+
     {
       :show       => 'get(:show, :id => "1")',
       :edit       => 'get(:edit, :id => "1")',
@@ -328,31 +327,31 @@ describe TasksController do
     }.each do |(action, code)|
       it "should not allow #{action}, and redirect_to root_url" do
         Task.should_receive(:find).with("1").and_return(mock_task)
-        
+
         eval code
       end
     end
-    
+
     {
       :create     => 'post(:create, :section_id => "1")',
       :archived   => 'get(:archived, :section_id => "1")'
     }.each do |(action, code)|
-     it "should not allow #{action}, and redirect_to root_url" do
-       Section.should_receive(:find).with("1").and_return(mock_section)
-      
-       eval code
-     end
+      it "should not allow #{action}, and redirect_to root_url" do
+        Section.should_receive(:find).with("1").and_return(mock_section)
+
+        eval code
+      end
     end
-    
+
     {
       :reorder    => 'get(:reorder, :project_id => "1")',
       :search     => 'get(:search, :project_id => "1")'
     }.each do |(action, code)|
-     it "should not allow #{action}, and redirect_to root_url" do
-       Project.should_receive(:find).with("1").and_return(mock_project)
-       
-       eval code
-     end
+      it "should not allow #{action}, and redirect_to root_url" do
+        Project.should_receive(:find).with("1").and_return(mock_project)
+
+        eval code
+      end
     end
   end
 end
