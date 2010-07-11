31547372a75f72df90a01d9a5d10f18b0c9e62bf
RStankov <rstankov@gmail.com>
2010-07-12 01:24:08 +0300

    add default Account into the db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+# create the first user for Taskar
+if User.count == 0
+  user = User.create(
+    :email                  => 'admin@taskar.com', 
+    :first_name             => 'Admin', 
+    :last_name              => 'Adminov', 
+    :password               => '123456', 
+    :password_confirmation  => '123456',
+    :admin                  => true
+  )
 
-user = User.create(
-  :email                  => 'admin@taskar.com', 
-  :first_name             => 'Admin', 
-  :last_name              => 'Adminov', 
-  :password               => '123456', 
-  :password_confirmation  => '123456',
-  :admin                  => true
-)
+  unless user.new_record?
+    p "User '#{user.email}' with password '#{user.password}' was created succesfully"
+  else
+    p "User failed to be created: #{user.errors.full_messages}" 
+  end
+end
 
-unless user.new_record?
-  p "User '#{user.email}' with password '#{user.password}' created succesfully"
-else
-  p "User failed to be created: #{user.errors.full_messages}" 
+# create first project for taskar
+if Account.count == 0
+  account = Account.new :name => "Taskar"
+  account.owner = User.first
+  if account.save
+    p "Account '#{account.name}' was created succesfully"
+  else
+    p "Account failed to be created: #{account.errors.full_messages}"
+  end
 end
