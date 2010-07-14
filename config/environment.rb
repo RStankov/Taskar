12f532558261954282be68d6b73c4c402d24ab27
RStankov <rstankov@gmail.com>
2010-07-14 21:03:06 +0300

    fix mailers with some default addresses

--- config/environment.rb
+++ config/environment.rb
@@ -49,4 +49,6 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :bg
+
+  config.action_mailer.default_url_options = { :host => "taskar.rstankov.com" }
 end
