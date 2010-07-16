4f198e222e6660c08111a6fcbdadd540998dd516
RStankov <rstankov@gmail.com>
2010-07-17 01:51:24 +0300

    add empty state for the dashboard

--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
-describe "/dashboard/index" do
-  before do
-    assigns[:projects] = [ Factory(:project) ]
+describe "/dashboard/index" do  
+  it "should render empty dashboard message" do
+    sign_in Factory(:user)
+    
+    assigns[:projects] = []
+    
+    render
+    
+    response.should have_tag('.empty_dashboard')
+    response.should_not have_tag('#events')
   end
   
-  it "should render" do
+  it "should render events table if projects exists" do
+    assigns[:projects] = [ Factory(:project) ]
+        
     render
+    
+    response.should_not have_tag('.empty_dashboard')
+    response.should have_tag('#events')
   end
 end
