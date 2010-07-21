bf802ee953060753ba7f826383f0c7c2f3abc30c
RStankov <rstankov@gmail.com>
2010-07-21 23:58:34 +0300

    reorganize users spec suite to member / collection specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,166 +9,156 @@ describe UsersController do
       @current_user.should_receive(:account).and_return mock_account
     end
 
-    describe "GET index" do
-      before do
-        mock_account.should_receive(:users).and_return([mock_user])
-        get :index
-      end
-
-      it { should assign_to(:users).with([mock_user]) }
-      it { should render_template("index") }
-    end
-
-    def mock_users_find_with(id)
-      users  = []
-      mock_account.should_receive(:users).and_return users
-      users.should_receive(:find).with(id).and_return mock_user
-    end
-
-    describe "GET show" do
-      before do
-        mock_users_find_with "15"
-        
-        get :show, :id => "15"
-      end
-
-      it { should assign_to(:user).with(mock_user) }
-      it { should render_template("show") }
-    end
-
-    describe "GET new" do
+    describe "on member action" do
       before do
         users  = []
         mock_account.should_receive(:users).and_return users
-        users.should_receive(:build).and_return User.new
-        
-        get :new
+        users.should_receive(:find).with("15").and_return mock_user
+      end
+      
+      describe "GET show" do
+        before do
+          get :show, :id => "15"
+        end
+
+        it { should assign_to(:user).with(mock_user) }
+        it { should render_template("show") }
       end
 
-      it { assigns[:user].should be_new_record }
-      it { should render_template("new") }
-    end
+      describe "GET edit" do
+        before do
+          get :edit, :id => "15"
+        end
 
-    describe "POST create" do
-      def user_params
-        {:these => "params"}
+        it { should assign_to(:user).with(mock_user) }
+        it { should render_template("edit") }
       end
 
-      def post_create(params = {})
-        post :create, {:user => user_params}.merge(params)
+      describe "PUT update" do
+        def user_params
+          {:these => 'params'}
+        end
+
+        def put_update(params = {})
+          put :update, {:id => 15, :user => user_params}.merge(params)
+        end
+
+        it "should render edit template when user is invalid" do
+          mock_user.should_receive(:update_attributes).with(user_params.stringify_keys).and_return(false)
+
+          put_update
+
+          should assign_to(:user).with(mock_user)
+          should render_template("edit")
+        end
+
+        it "should redirect to users page, when user is valid" do
+          mock_user.should_receive(:update_attributes).with(user_params.stringify_keys).and_return(true)
+
+          put_update
+
+          should assign_to(:user).with(mock_user)
+          should redirect_to(user_url(mock_user))  
+        end
       end
 
-      before do
-        users  = []
-        mock_account.should_receive(:users).and_return users
-        users.should_receive(:build).with(user_params.stringify_keys).and_return mock_user
+      describe "DELETE destroy" do
+        before do
+          mock_user.should_receive(:destroy)
+
+          delete :destroy, :id => "15"
+        end
+
+        it { should redirect_to(users_url) }
       end
 
-      it "should render new template when user is invalid" do
-        mock_user.should_receive(:save).and_return(false)
+      describe "PUT set_admin" do
+        it "should set user admin field to param[:value]" do
+          mock_user.should_receive(:admin=).with(true)
+          mock_user.should_receive(:save)
 
-        post_create
+          put :set_admin, :id => "15", :admin => true
+        end
 
-        should assign_to(:user).with(mock_user)
-        should render_template("new")
+        it "should not allow the current user to change his admin status" do
+          mock_user.should_receive(:==).with(@current_user).and_return true
+          mock_user.stub(:admin?).and_return(true)
+
+          mock_user.should_not_receive(:admin=)
+          mock_user.should_not_receive(:save)
+
+          put :set_admin, :id => "15", :admin => true
+        end
+
+        it "should redirect to user page" do
+          mock_user.stub!(:admin=)
+          mock_user.stub!(:save)
+
+          put :set_admin, :id => "15"
+
+          should redirect_to(user_url(mock_user))
+        end
       end
 
-      it "should redirect to users page, when user is valid" do
-        mock_user.should_receive(:save).and_return(true)
-
-        post_create
-
-        should assign_to(:user).with(mock_user)
-        should redirect_to(user_url(mock_user))
-      end
-    end
-
-    describe "GET edit" do
-      before do
-        mock_users_find_with "15"
-        
-        get :edit, :id => "15"
-      end
-
-      it { should assign_to(:user).with(mock_user) }
-      it { should render_template("edit") }
-    end
-
-    describe "PUT update" do
-      def user_params
-        {:these => 'params'}
-      end
-
-      def put_update(params = {})
-        put :update, {:id => 15, :user => user_params}.merge(params)
-      end
-
-      before do
-        mock_users_find_with "15"
-      end
-
-      it "should render edit template when user is invalid" do
-        mock_user.should_receive(:update_attributes).with(user_params.stringify_keys).and_return(false)
-
-        put_update
-
-        should assign_to(:user).with(mock_user)
-        should render_template("edit")
-      end
-
-      it "should redirect to users page, when user is valid" do
-        mock_user.should_receive(:update_attributes).with(user_params.stringify_keys).and_return(true)
-
-        put_update
-
-        should assign_to(:user).with(mock_user)
-        should redirect_to(user_url(mock_user))  
-      end
-    end
-
-    describe "DELETE destroy" do
-      before do
-        mock_users_find_with "15"
-        mock_user.should_receive(:destroy)
-
-        delete :destroy, :id => "15"
-      end
-
-      it { should redirect_to(users_url) }
-    end
-
-    describe "PUT set_admin" do
-      before do
-        mock_users_find_with "15"
-      end
-
-      it "should set user admin field to param[:value]" do
-        mock_user.should_receive(:admin=).with(true)
-        mock_user.should_receive(:save)
-
-        put :set_admin, :id => "15", :admin => true
-      end
-
-      it "should not allow the current user to change his admin status" do
-        mock_user.should_receive(:==).with(@current_user).and_return true
-        mock_user.stub(:admin?).and_return(true)
-
-        mock_user.should_not_receive(:admin=)
-        mock_user.should_not_receive(:save)
-
-        put :set_admin, :id => "15", :admin => true
-      end
-
-      it "should redirect to user page" do
-        mock_user.stub!(:admin=)
-        mock_user.stub!(:save)
-
-        put :set_admin, :id => "15"
-
-        should redirect_to(user_url(mock_user))
-      end
     end
     
+    describe "on collection action" do
+      before do
+        mock_account.should_receive(:users).and_return @users = [mock_user]
+      end
+      
+      describe "GET index" do
+        before do
+          get :index
+        end
+
+        it { should assign_to(:users).with(@users) }
+        it { should render_template("index") }
+      end
+      
+      describe "GET new" do
+        before do
+          @users.should_receive(:build).and_return User.new
+
+          get :new
+        end
+
+        it { assigns[:user].should be_new_record }
+        it { should render_template("new") }
+      end
+      
+      describe "POST create" do
+        def user_params
+          {:these => "params"}
+        end
+
+        def post_create(params = {})
+          post :create, {:user => user_params}.merge(params)
+        end
+
+        before do
+          @users.should_receive(:build).with(user_params.stringify_keys).and_return mock_user
+        end
+
+        it "should render new template when user is invalid" do
+          mock_user.should_receive(:save).and_return(false)
+
+          post_create
+
+          should assign_to(:user).with(mock_user)
+          should render_template("new")
+        end
+
+        it "should redirect to users page, when user is valid" do
+          mock_user.should_receive(:save).and_return(true)
+
+          post_create
+
+          should assign_to(:user).with(mock_user)
+          should redirect_to(user_url(mock_user))
+        end
+      end
+    end
   end
   
   describe "with normal user" do
