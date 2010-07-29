44bf622be436a0fbb1fdec6578b9931a1a711d10
RStankov <rstankov@gmail.com>
2010-07-29 23:06:11 +0300

    add links for statuses in the events table

--- spec/views/sections/index.html.erb_spec.rb
+++ spec/views/sections/index.html.erb_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe "/sections/index.html.erb" do
   before do
-    assigns[:events] = [ Factory(:event) ].paginate
+    assigns[:events]  = [ Factory(:event) ].paginate
+    assigns[:project] = mock_project
   end
 
   it "should renders" do
--- spec/views/statuses/index.html.erb_spec.rb
+++ spec/views/statuses/index.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "/statuses/index" do
   before do
     assigns[:statuses] = [Factory(:status)].paginate
+    assigns[:project] = mock_project
   end
 
   it { render }
