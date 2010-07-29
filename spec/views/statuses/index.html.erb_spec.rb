f27d7ff2f045f9e2d219d58de50a5c81f45fe234
RStankov <rstankov@gmail.com>
2010-07-29 22:25:17 +0300

    design statuses#index

--- spec/views/statuses/index.html.erb_spec.rb
+++ spec/views/statuses/index.html.erb_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe "/statuses/index" do
-  before(:each) do
-    render 'statuses/index'
+  before do
+    assigns[:statuses] = [Factory(:status)].paginate
   end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/statuses/index])
-  end
+  it { render }
 end
