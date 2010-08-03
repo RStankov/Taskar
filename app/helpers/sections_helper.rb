7c92356cee5878cd03e4f7e39005fd1ac1bfcedd
RStankov <rstankov@gmail.com>
2010-08-04 00:49:05 +0300

    handle the long status case

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -6,4 +6,26 @@ module SectionsHelper
       link_to("&nbsp;", new_project_section_path(@project, :before => before))
     end
   end
+  
+  def participant_last_action(participant)
+    t :".last_activity", :time => time_ago_in_words(participant.user.last_active_at)
+  end
+  
+  def participant_status_title(participant)
+    if participant.status
+      if participant.status.size < 100
+        participant_last_action participant
+      else
+        h(participant.status) + "\n\n" + participant_last_action(participant)
+      end
+    end
+  end
+  
+  def participant_status(participant)
+    if participant.status
+      simple_format h(participant.status)
+    else  
+      participant_last_action participant
+    end
+  end
 end
