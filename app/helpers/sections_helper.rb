bec3f91e96098017585284b101dcabf3d344828c
RStankov <rstankov@gmail.com>
2010-08-10 22:19:22 +0300

    user helper for showing user tasks count

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -2,13 +2,18 @@ module SectionsHelper
   def new_section_link(before = nil)
     before = before.id.to_s if before
     
-    content_tag(:li, :class => "add_section", :"data-before" => before) do
+    content_tag(:li, :class => "add_section", "data-before" => before) do
       link_to("&nbsp;", new_project_section_path(@project, :before => before))
     end
   end
   
+  
+  def participant_task_count(count)
+    t "layouts.sections.tasks", :count => count
+  end
+  
   def participant_last_action(participant)
-    t :"layouts.sections.last_activity", :time => time_ago_in_words(participant.user.last_active_at)
+    t "layouts.sections.last_activity", :time => time_ago_in_words(participant.user.last_active_at)
   end
   
   def participant_status_title(participant)
