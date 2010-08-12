cf694270866ed2c89bb14d4a12b41bd2cc4516ef
RStankov <rstankov@gmail.com>
2010-08-12 23:31:24 +0300

    show delete button only to project user

--- spec/views/statuses/index.html.erb_spec.rb
+++ spec/views/statuses/index.html.erb_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "/statuses/index" do
   before do
+    sign_in Factory(:user) 
+        
     assigns[:statuses] = [Factory(:status)].paginate
     assigns[:project] = mock_project
   end
