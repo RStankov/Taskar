dbae7c61bd98fdc727dda9fedfe8c7794bbe66f1
RStankov <rstankov@gmail.com>
2010-08-19 23:16:55 +0300

    very strange! this was failing and then passing

--- spec/views/sections/index.html.erb_spec.rb
+++ spec/views/sections/index.html.erb_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe "/sections/index.html.erb" do
-  before do
-    assigns[:events]  = [ Factory(:event) ].paginate
-    assigns[:project] = Factory(:project)
-  end
-
   it "should renders" do
+    assigns[:events]  = [ Factory(:event) ].paginate
+    assigns[:project] = mock_project
+    
     render
   end
 end
