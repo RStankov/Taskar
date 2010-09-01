511e17448dfcec6ff50fa52d19df0ccdd504fb87
RStankov <rstankov@gmail.com>
2010-09-02 00:51:46 +0300

    smooth the js for the projects list

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -28,28 +28,56 @@ Graphics.Colors = {
 	  return angle + "-" + mask[0] + "-" + mask[1];
 	}
 };
-Graphics.createPieChart = function(dataSeeder){
-  var holder  = new Element('div'),
-      colors  = dataSeeder.select('[data-color]').invoke('getAttribute', 'data-color'),
-      labels  = dataSeeder.select('strong').pluck('innerHTML'),
-      values	= dataSeeder.select('em').map(function(element){ return parseInt(element.innerHTML, 10); }).reject(function(v){ return v <= 0; }),
-      size    = dataSeeder.getWidth();
-
-  if (values.length < 2){
+Graphics.createPieChart = function(container){
+  var seeder  = container.down('ul');
+  
+  if (!seeder){
     return;
   }
   
-  var center = size/2
-      radius = size/2.4,
-      angle  = 0,
-      rad    = Math.PI / 180,
-      paper  = new Raphael(holder, size, size),
-      chart  = paper.set(),
-      total  = values.reduce(function(total, value){ return total + value });
+  var size    = container.getWidth(),
+      holder  = new Element('div'),
+      paper   = new Raphael(holder, size, size);
+      
+  var colors  = container.select('[data-color]').invoke('getAttribute', 'data-color'),
+      labels  = container.select('strong').pluck('innerHTML'),
+      values	= container.select('em').map(function(element){ return parseInt(element.innerHTML, 10); }).reject(function(v){ return v <= 0; });
+  
+  if (values.length < 2){
+    paper
+      .path("M29.124,12.75c-0.004-2.208-1.792-3.997-3.999-4V8.749H12.868c-0.505-1.622-2.011-2.808-3.805-2.811H6.188c-2.208,0.002-3.997,1.792-4.001,4v14.188c0.004,2.206,1.793,3.995,4.001,3.999h18.938c2.205-0.004,3.995-1.793,3.999-3.999V12.75zM6.188,7.937h2.875c1.046-0.004,1.917,0.834,1.983,1.876l0.058,0.937h14.022c1.093,0.002,1.997,0.906,1.999,2v0.495c-0.591-0.345-1.268-0.557-2-0.558H6.187c-0.732,0.001-1.41,0.214-2,0.559V9.937C4.19,8.843,5.094,7.939,6.188,7.937zM25.125,26.125H6.188c-1.093-0.002-1.997-0.908-2.001-2v-7.438h0.001c0.002-1.095,0.906-1.999,2-2.001h18.938c1.093,0.002,1.991,0.901,2,1.991v7.447C27.122,25.219,26.218,26.123,25.125,26.125z")    
+      .attr({fill: "#000", stroke: "none"})
+      .translate(60, 60)
+      .scale(4, 4)
+      .mouseover(mouseover2)
+      .mousemove(mousemove)
+      .mouseout(mouseout2)
+      .title = "No tasks"
+  
+    seeder.insert({before: holder})
+    seeder.remove();
+
+    return;  
+  }
+
+  var center  = size/2
+      radius  = size/2.4,
+      angle   = 0,
+      rad     = Math.PI / 180,
+      chart   = paper.set(),
+      total   = values.reduce(function(total, value){ return total + value });
 
   function mouseover(e){
     this.animate({scale: [1.1, 1.1, center, center]}, 500, "elastic");
     
+    $('info_bubble').update(this.title).setStyle({
+      top:  Event.pointerY(e) + 20 + 'px',
+      left: Event.pointerX(e) + 10 + 'px'
+    }).show();
+  }
+  
+  function mouseover2(e){    
+    this.animate({scale: [4.4, 4.4, center, center]}, 500, "elastic");
     $('info_bubble').update(this.title).setStyle({
       top:  Event.pointerY(e) + 20 + 'px',
       left: Event.pointerX(e) + 10 + 'px'
@@ -61,6 +89,11 @@ Graphics.createPieChart = function(dataSeeder){
         top:  Event.pointerY(e) + 20 + 'px',
         left: Event.pointerX(e) + 10 + 'px'
       });
+  }
+  
+  function mouseout2(){
+    this.animate({scale: [4, 4, center, center]}, 500, "elastic");
+    $('info_bubble').hide();
   }
 
   function mouseout(){
@@ -90,8 +123,8 @@ Graphics.createPieChart = function(dataSeeder){
     return path;
   }));
   
-  dataSeeder.insert({before: holder})
-  dataSeeder.remove();
+  seeder.insert({before: holder})
+  seeder.remove();
 };
 
-$$('li.project ul').each(Graphics.createPieChart);
+$$('li.project').each(Graphics.createPieChart);
