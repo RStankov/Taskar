7e61a2dc564536950624aabc8a61cb1e82ddae64
RStankov <rstankov@gmail.com>
2010-09-20 23:16:03 +0300

    add data-disable attribute to forms (use it on status forms)

--- app/javascripts/behaviors.js
+++ app/javascripts/behaviors.js
@@ -1,8 +1,10 @@
 CD3.Behaviors({
   'body:ajax:after': {
     '.action_form':       function(e, element){
-                            element.addClassName('loading');
-                            element.getElements().invoke('disable');
+                            if (element.getAttribute('data-disable') !== 'false'){
+                              element.addClassName('loading');
+                              element.getElements().invoke('disable');
+                            }
                           }
   },
   '#sections':            function(){
