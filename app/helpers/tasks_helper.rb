a7f24884a5527a190096408849e015b25fce9bce
RStankov <rstankov@gmail.com>
2010-09-23 22:16:36 +0300

    add html_safe to tasks helpers

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,20 +1,20 @@
 module TasksHelper
   def task_state_checkbox(task)
     state = task.state
-    
+
     attributes = 'class="checkbox ' + state + '" data-state="' + state + '" data-url="' + state_task_path(task) + '"'
     attributes << ' data-disabled="true"' if task.archived?
-    
-    "<span #{attributes}></span>"
+
+    "<span #{attributes}></span>".html_safe!
   end
-  
+
   def task_description(task)
     name    = "short"
     options = {
       :from => task.user.full_name,
       :on   => time_ago_in_words(task.created_at)
     }
-    
+
     if task.responsible_party
       if task.responsible_party == task.user
         name = "same"
@@ -23,7 +23,7 @@ module TasksHelper
         options[:to] = task.responsible_party.full_name
       end
     end
-    
-    t "tasks.show.description.#{name}", options
+
+    t("tasks.show.description.#{name}", options)
   end
 end
