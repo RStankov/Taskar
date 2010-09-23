a187892460dde27deeb00f3e4a0fae82d1940759
RStankov <rstankov@gmail.com>
2010-09-24 01:14:09 +0300

    install rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskar::Application.config.secret_token = '52e514075beefa573e89e98f9e460fbab45d938981243c6dd186884a045218d34c16a2d6a6240b270d13b37dd67df47a32a605d73d602e45f37a59f1176f8fb1'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_Taskar_session',
-  :secret      => '513dfdd0bedcea050f98df362e2c97226f7b73827ea9813fa97cebf584d6ca48d5fb1324ea00c820722fc8306955b7bb47b8a8c65972cfdd7a9c2edb1c04a6b4'
-}
+Taskar::Application.config.session_store :cookie_store, :key => '_Taskar_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Taskar::Application.config.session_store :active_record_store
