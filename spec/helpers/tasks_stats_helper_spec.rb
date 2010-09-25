c72b52e2e741dd8ff33c58cdf719fec21d52a361
RStankov <rstankov@gmail.com>
2010-09-26 00:50:14 +0300

    fix forgotten view spec

--- spec/helpers/tasks_stats_helper_spec.rb
+++ spec/helpers/tasks_stats_helper_spec.rb
@@ -3,29 +3,31 @@ require 'spec_helper'
 describe TasksStatsHelper do
   describe "#tasks_stats_subject_name" do
     MAX_LENGTH = TasksStatsHelper::MAX_SUBJECT_LENGTH_NAME
-    
+
     context "when subject.name is smaller then #{MAX_LENGTH} chars" do
       before { @subject = mock(Object, :name => "a" * MAX_LENGTH) }
-      
+
       it "returns just <span>subject.name</span>" do
         helper.tasks_stats_subject_name(@subject).should == "<span>#{@subject.name}</span>"
       end
     end
-    
+
     context "when subject.name is more than #{MAX_LENGTH} chars" do
       before { @subject = mock(Object, :name => "a" * MAX_LENGTH + "b") }
-            
+
       it "uses truncate with :length => #{MAX_LENGTH}" do
         helper.should_receive(:truncate).with(@subject.name, :length => MAX_LENGTH)
-        
+
         helper.tasks_stats_subject_name(@subject)
       end
+
       it "returns the <span>truncated subject name</span>" do
         truncated_text = helper.truncate(@subject.name, :length => MAX_LENGTH)
-        helper.tasks_stats_subject_name(@subject).should have_tag("span", truncated_text)
+        helper.tasks_stats_subject_name(@subject).should have_selector("span", :content => truncated_text)
       end
+
       it "returns the span[title=subject.name]" do
-        helper.tasks_stats_subject_name(@subject).should have_tag("span[title=#{@subject.name}]")
+        helper.tasks_stats_subject_name(@subject).should have_selector("span[title=\"#{@subject.name}\"]")
       end
     end
   end
