2894e7afb2761f5384ccfea5dddc50db5f8d015a
RStankov <rstankov@gmail.com>
2010-09-26 21:13:35 +0300

    Rails3 use .field_with_errors instead of .fieldWithErrors

--- app/javascripts/taskar/sections.js
+++ app/javascripts/taskar/sections.js
@@ -4,7 +4,7 @@ Taskar.Sections = {
     if (input.getValue().trim().length == 0){
       e.stop();
       Taskar.FX.shake(input, {distance: 2, turns: 2});
-      
+
       input.style.backgroundColor = null;
       input.highlight({startColor: Taskar.ERROR_COLOR});
       input.focus();
@@ -13,28 +13,28 @@ Taskar.Sections = {
   resetTaskForm: function(form, before, dontPersistResponsible){
     var resposible    = form.down('select[name*=responsible_party_id]'),
         previousValue = resposible && resposible.getValue();
-    
+
     this.resetForm(form);
-    
+
     !dontPersistResponsible && resposible && resposible.setValue(previousValue);
-    
+
     if (before !== false){
       form.down('input[name*=insert_after]').setValue(before || null);
     }
-    
+
     form.visible() && form.down('textarea').focus();
   },
   resetForm: function(form){
     form.reset();
-    
+
     var errors = form.down('.error_messages');
     if (errors){
       errors.remove();
-      form.select('.fieldWithErrors').each(function(element){
+      form.select('.field_with_errors').each(function(element){
         element.replace(element.down());
       });
     }
-    
+
     form.removeClassName('loading');
     form.getElements().invoke('enable');
   },
