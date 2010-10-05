3b463c38bef51bc02b212e14bd02ee12b3efb1ae
RStankov <rstankov@gmail.com>
2010-10-05 22:54:31 +0300

    added Account.find_id_by_name

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,18 @@
 class Account < ActiveRecord::Base
   belongs_to :owner, :class_name => "User"
-  
+
   validates_presence_of :name
-  
+
   validates_uniqueness_of :name
-  
+
   attr_accessible :name
-  
+
   has_many :users,    :dependent => :destroy
   has_many :projects, :dependent => :destroy
+
+  def self.find_id_by_name(name)
+    if account = where(:name => name).first
+      account.id
+    end
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe Account do
     it_should_allow_mass_assignment_only_of :name
-    
+
     it { should belong_to(:owner) }
     it { should validate_presence_of(:name) }
     it { Factory(:account).should validate_uniqueness_of(:name) }
-    
+
     it { should have_many(:users) }
     it { should have_many(:projects) }
+
+    describe "find_id_by_name" do
+      it "should find account id for given name" do
+        account = Factory(:account)
+        Account.find_id_by_name(account.name).should == account.id
+      end
+
+      it "should return nil if accunt doesn't exists" do
+        Account.find_id_by_name("not - existing - name").should be_nil
+      end
+    end
 end
