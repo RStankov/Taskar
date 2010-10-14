eb09407ca567fdd7db09bfc6017a9b7220333d4b
RStankov <rstankov@gmail.com>
2010-10-14 23:30:52 +0300

    we need account_user for the original user

--- db/migrate/20101010210031_merge_users_with_same_emails.rb
+++ db/migrate/20101010210031_merge_users_with_same_emails.rb
@@ -3,6 +3,8 @@ class MergeUsersWithSameEmails < ActiveRecord::Migration
     User.group(:email).each do |user|
       p "merge - #{user.email} (#{user.id})"
 
+      AccountUser.create :account_id => user.account_id, :user_id => user.id
+
       User.where(:email => user.email).where("id != ?", user.id).each do |duplicate|
         duplicate.events.update_all                   :user_id => user.id
         duplicate.comments.update_all                 :user_id => user.id
