937364300a15bdb75e4d1fb20745d08119a3b9f4
RStankov <rstankov@gmail.com>
2010-10-14 23:35:49 +0300

    account_id is attr_protected so AccountUser.create is not option

--- db/migrate/20101010210031_merge_users_with_same_emails.rb
+++ db/migrate/20101010210031_merge_users_with_same_emails.rb
@@ -3,7 +3,11 @@ class MergeUsersWithSameEmails < ActiveRecord::Migration
     User.group(:email).each do |user|
       p "merge - #{user.email} (#{user.id})"
 
-      AccountUser.create :account_id => user.account_id, :user_id => user.id
+      # AccountUser#account_id is attr_protected, so AccountUser.create is not option
+      account = AccountUser.new
+      account.account_id = user.account_id
+      account.user_id = user.id
+      account.save
 
       User.where(:email => user.email).where("id != ?", user.id).each do |duplicate|
         duplicate.events.update_all                   :user_id => user.id
@@ -17,7 +21,11 @@ class MergeUsersWithSameEmails < ActiveRecord::Migration
           duplicate.owned_account.update_attribute :owner_id, user.id
         end
 
-        AccountUser.create :account_id => duplicate.account_id, :user_id => user.id
+         # AccountUser#account_id is attr_protected, so AccountUser.create is not option
+        account = AccountUser.new
+        account.account_id = duplicate.account_id
+        account.user_id = user.id
+        account.save
 
         duplicate.destroy
 
