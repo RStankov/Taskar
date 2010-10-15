6ec1795cb9e433e8cf7cd161bab21b8a652cd8ae
RStankov <rstankov@gmail.com>
2010-10-16 01:42:53 +0300

    better spec name

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Project do
   it_should_allow_mass_assignment_only_of :name, :user_ids
-  
+
   it { should validate_presence_of(:name) }
   it { Factory(:project).should validate_uniqueness_of(:name).scoped_to(:account_id) }
-  
+
   it { should have_many(:sections) }
   it { should have_many(:participants) }
   it { should have_many(:users) }
@@ -15,17 +15,17 @@ describe Project do
   it { should have_many(:statuses) }
   it { should belong_to(:account) }
 
-  describe "involves?" do
+  describe "#involves?" do
     before do
       @project = Factory(:project)
       @user    = Factory(:user)
     end
-    
+
     it "should return true if user is involved in the project" do
       @project.users << @user
       @project.involves?(@user).should be_true
     end
-    
+
     it "should return false if user is not involved in the project" do
       @project.involves?(@user).should be_false
     end
