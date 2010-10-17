caab47a1844be8998194add6804afc7486e13fb3
RStankov <rstankov@gmail.com>
2010-10-17 23:40:36 +0300

    i18n fixes

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,8 @@ class Account < ActiveRecord::Base
 
   attr_accessible :name
 
+  attr_readonly :owner_id
+
   has_many :users, :through => :account_users
   has_many :account_users, :dependent => :destroy
   has_many :projects, :dependent => :destroy
