4a3c4cab49b2bd6d79e979db9d2bf193ba0515e0
RStankov <rstankov@gmail.com>
2010-10-23 21:00:49 +0300

    this must be show action

--- spec/routing/sign/invitations_spec.rb
+++ spec/routing/sign/invitations_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Sign::InvitationsController do
   describe "routing" do
     it "recognizes and generates #show" do
-      { :get => "sign/invitations/some-hash" }.should route_to(:controller => "sign/invitations", :action => "new", :id => "some-hash")
+      { :get => "sign/invitations/some-hash" }.should route_to(:controller => "sign/invitations", :action => "show", :id => "some-hash")
     end
 
     it "recognizes and generates #update" do
