fad4679f7e1cdc57e6d0213e76ad2da19e417452
RStankov <rstankov@gmail.com>
2010-10-24 00:21:04 +0300

    add password only form

--- spec/views/devise/invitations/show.html.erb_spec.rb
+++ spec/views/devise/invitations/show.html.erb_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
 describe "devise/invitations/show.html.erb" do
-  before { assign :invitation, Factory.stub(:invitation, :token => "foo", :account => Factory.stub(:account, :owner => Factory.stub(:user))) }
+  before do
+    assign :invitation, @invitation = Factory.stub(:invitation, {
+      :token   => "foo",
+      :account => Factory.stub(:account, :owner => Factory.stub(:user))
+    })
+    @invitation.stub(:user).and_return Factory.stub(:user)
+  end
 
-  it { render }
+  it "should render new user form if invitation.user is new" do
+    @invitation.user.stub(:new_record?).and_return true
+
+    render
+    rendered.should have_selector("#user_password")
+    rendered.should have_selector("#user_password_confirmation")
+    rendered.should have_selector("#user_locale")
+    rendered.should have_selector("#user_avatar")
+  end
+
+  it "should render sign_in form if invitation.user is not new" do
+    @invitation.user.stub(:new_record?).and_return false
+
+    render
+    rendered.should have_selector("#user_password")
+    rendered.should_not have_selector("#user_password_confirmation")
+    rendered.should_not have_selector("#user_locale")
+    rendered.should_not have_selector("#user_avatar")
+  end
 end
