3739958b5fe2c6ddbc39d674623dae4c36f23cb7
RStankov <rstankov@gmail.com>
2010-10-24 22:56:12 +0300

    rename sign routing specs

--- spec/routing/sign/invitations_routing_spec.rb
+++ spec/routing/sign/invitations_routing_spec.rb
@@ -9,9 +9,5 @@ describe Sign::InvitationsController do
     it "recognizes and generates #update" do
       { :put => "sign/invitations/some-hash" }.should route_to(:controller => "sign/invitations", :action => "update", :id => "some-hash")
     end
-
-    it "recognizes and generates #destroy" do
-      { :delete => "sign/invitations/some-hash" }.should route_to(:controller => "sign/invitations", :action => "destroy", :id => "some-hash")
-    end
   end
 end
