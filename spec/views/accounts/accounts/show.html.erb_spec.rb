307c73497639791a75c55dccf2a60f0e18228e61
RStankov <rstankov@gmail.com>
2010-10-24 17:57:12 +0300

    show empty message in accounts dashboards

--- spec/views/accounts/accounts/show.html.erb_spec.rb
+++ spec/views/accounts/accounts/show.html.erb_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 describe "/accounts/accounts/show.html.erb" do
   before do
-    assign :account, Factory(:account)
+    assign :account, Factory.stub(:account)
   end
 
-  it { render }
+  it "should render events table (if there is atleast one project)" do
+    assign :projects, [Factory.stub(:project)]
+
+    render
+    rendered.should_not have_selector('.help_notice')
+    rendered.should have_selector('#events')
+  end
+
+  it "should render .help_notice (if there isn't any project)" do
+    assign :projects, []
+
+    render
+    rendered.should have_selector('.help_notice')
+    rendered.should_not have_selector('#events')
+  end
 end
