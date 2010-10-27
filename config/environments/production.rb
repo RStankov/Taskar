22249d1acff4de1c0617e2f14f9b28225de1dc31
RStankov <rstankov@gmail.com>
2010-10-27 23:16:13 +0300

    an other smtp options try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,16 @@ Taskar::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "localhost",
+    :port                 => 25,
+    :domain               => "localhost",
+    :user_name            => nil,
+    :password             => nil,
+    :authentication       => :plain,
+    :enable_starttls_auto => false
+  }
+
 
   # Enable threaded mode
   # config.threadsafe!
