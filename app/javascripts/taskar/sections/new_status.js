6244a36bf696fa8d3b6e10cbf93f2200690d9604
RStankov <rstankov@gmail.com>
2010-10-29 01:01:10 +0300

    new status form to re-use the WindowForm js

--- app/javascripts/taskar/sections/new_status.js
+++ app/javascripts/taskar/sections/new_status.js
@@ -1,19 +1,3 @@
 Taskar.Sections.NewStatus = function(userCard){
-  var form = $('new_status');
-  
-  function hide(){
-    form.fade();
-  }
-  
-  userCard.down('img').observe('click', function(){
-    form.down('textarea').setValue('').setStyle({backgroundColor: null});
-    form.toggleWithEffect('appear', function(e){
-      e.element.visible() && e.element.down('textarea').setValue('').focus();
-    });
-  });
-  
-  form.observe('submit', Taskar.Sections.validateForm);
-  form.observe('key:esc', hide);
-  form.observe('ajax:post', hide);
-  form.down('input[type=button]').observe('click', hide);
+  userCard.down("img").observe("click", Taskar.UI.WindowForm("new_status"));
 };
