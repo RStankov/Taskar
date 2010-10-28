9f462c0735510192ef02330c020904ff7729f472
RStankov <rstankov@gmail.com>
2010-10-29 01:49:40 +0300

    remove the unneeded CD3.Widgets extentions

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -5,7 +5,9 @@
 var CD3 = {};
 
 //= require <cd3/behaviors>
-//= require <cd3/extensions>
+//= require <cd3/extensions/element>
+//= require <cd3/extensions/element/insert>
+//= require <cd3/extensions/effects>
 
 //= require "extensions"
 //= require "rails"
