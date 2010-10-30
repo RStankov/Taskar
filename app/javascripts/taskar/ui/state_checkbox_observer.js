88586e708f229ca9355a60914500aadd2a3975e9
RStankov <rstankov@gmail.com>
2010-10-31 02:14:06 +0300

    better and faster state resolving in StateCheckboxObserver

--- app/javascripts/taskar/ui/state_checkbox_observer.js
+++ app/javascripts/taskar/ui/state_checkbox_observer.js
@@ -9,46 +9,42 @@ Taskar.UI.StateCheckboxObserver = function(){
 };
 
 Taskar.UI.StateCheckboxObserver.callback = (function(){
-  function nextState(state){
-    switch(state){
-      case 'opened':    return 'completed';
-      case 'completed': return 'rejected';
-      case 'rejected':  return 'opened';
-    }
-    return 'opened';
-  }
-  
+  var STATES = {
+    'opened':     'completed',
+    'completed':  'rejected',
+    'rejected':   'opened'
+  };
+
   function changeState(element){
     var current = element.getAttribute('data-state'),
-        next    = nextState(current),
+        next    = STATES[current] || 'opened',
         parent  = element.up('.task');
-    
+
     element.setAttribute('data-state', next);
     element.replaceClassName(current, next);
-    
+
     if (parent){
       parent.replaceClassName(current, next);
     }
   }
-  
+
   function clearTimer(element){
     var timer = element.retrieve('state:timer');
-    if (timer){ 
+    if (timer){
       clearTimeout(timer);
       element.store('state:timer', null);
     }
   }
-  
+
   function setTimer(element){
     element.store('state:timer', setTimeout(function(){
-      element.store('state:timer', null);
-      element.fire('state:changed', {state: element.getAttribute('data-state')});
-    }, 500));    
+      element.store('state:timer', null).fire('state:changed', {state: element.getAttribute('data-state')});
+    }, 500));
   }
-  
+
   return function(e, element){
-    changeState(element);
     clearTimer(element);
+    changeState(element);
     setTimer(element);
   };
 })();
