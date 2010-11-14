cd84887eabcaa8c52a493a1de395340f3c42c244
RStankov <rstankov@gmail.com>
2010-11-14 18:23:12 +0200

    add stubed task and new comment as assigned variables for comments#new

--- spec/views/comments/new.js.erb_spec.rb
+++ spec/views/comments/new.js.erb_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe "/comments/new.js.erb" do
   before do
-    assign :comment, Factory(:comment)
+    assign :comment, Comment.new
+    assign :task, Factory.stub(:task)
   end
 
   it { render }
