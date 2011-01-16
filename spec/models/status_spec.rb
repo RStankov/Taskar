478dbe4fc398a782a36f9c056fa0afa3d84ae86f
RStankov <rstankov@gmail.com>
2011-01-16 11:44:29 +0100

    Clean the status_spec spec

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe Status do
   it_should_allow_mass_assignment_only_of :text
-  
+
   it { should belong_to(:project) }
   it { should belong_to(:user) }
   it { should have_one(:event) }
   it { should validate_presence_of(:project) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:text) }
-  
-  it "should set project_user status to be this status on creation" do
+
+  it "sets project_user status to be this status on creation" do
     user          = Factory(:user)
     project       = Factory(:project)
     project_user  = Factory(:project_user, :user => user, :project => project)
     status        = Factory(:status, :text => "Status text", :user => user, :project => project)
-    
+
     project_user.status.should be_nil
     project_user.reload.status.should == "Status text"
   end
