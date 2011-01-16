73c723510bf356ec556d70afffb83215f951acaa
RStankov <rstankov@gmail.com>
2011-01-16 11:42:24 +0100

    Clean the comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,17 +16,19 @@ describe Comment do
   it_should_have_counter_cache_of :task
   it_should_have_counter_cache_of :user
 
-  it "should inherit the project_id from its parent task" do
+  let(:comment) { Factory(:comment) }
+  let(:user) { Factory(:user) }
+
+  it "inherits the project_id from its parent task" do
     task = Factory(:task)
-    user = Factory(:user)
 
     comment = user.new_comment(task, :text => "text")
 
     comment.save.should be_true
-    comment.project_id.should  == comment.project_id
+    comment.project_id.should == comment.project_id
   end
 
-  it "should not be added to archived task" do
+  it "can't be added to archived task" do
     task    = Factory(:task, :status => 1, :archived => true)
     comment = Factory.build(:comment, :task => task)
 
@@ -35,37 +37,29 @@ describe Comment do
   end
 
   describe "#editable_by?" do
-    before do
-      @comment = Factory(:comment)
+    it "returns false for user who is not comment creator" do
+      comment.should_not be_editable_by(user)
     end
 
-    it "should be false for user who is not comment creator" do
-      @comment.editable_by?(Factory(:user)).should be_false
+    it "returns true for the current user (if updated_at in Comment::EDITABLE_BY time)" do
+      comment.updated_at = Time.now
+      comment.should be_editable_by(comment.user)
     end
 
-    it "should be true for the current user (if updated_at in Comment::EDITABLE_BY time)" do
-      @comment.updated_at = Time.now
-      @comment.editable_by?(@comment.user).should be_true
-    end
-
-    it "should be false for the current user (if updated_at out of Comment::EDITABLE_BY time)" do
-      @comment.updated_at = Time.now - 2 * Comment::EDITABLE_BY
-      @comment.editable_by?(@comment.user).should be_false
+    it "returns false for the current user (if updated_at out of Comment::EDITABLE_BY time)" do
+      comment.updated_at = Time.now - 2 * Comment::EDITABLE_BY
+      comment.should_not be_editable_by(comment.user)
     end
   end
 
   describe "#editable_for" do
-    before do
-      @comment = Factory(:comment)
+    it "returns diffrence between Comment::EDITABLE_BY and updated_at " do
+      comment.editable_for.should == (((comment.updated_at + Comment::EDITABLE_BY) - Time.now) / 60).ceil
     end
 
-    it "should return diffrence between Comment::EDITABLE_BY and updated_at " do
-      @comment.editable_for.should == (((@comment.updated_at + Comment::EDITABLE_BY) - Time.now) / 60).ceil
-    end
-
-    it "should return 0 if this is not editable" do
-      @comment.updated_at = Time.now - 2 * Comment::EDITABLE_BY
-      @comment.editable_for.should == 0
+    it "returns 0 if this is not editable" do
+      comment.updated_at = Time.now - 2 * Comment::EDITABLE_BY
+      comment.editable_for.should == 0
     end
   end
 end
