7fae17341aac9a41fdf026e38f9eb2d828966add
RStankov <rstankov@gmail.com>
2011-01-22 23:26:08 +0100

    Improve projects specs

--- spec/controllers/accounts/projects_controller_spec.rb
+++ spec/controllers/accounts/projects_controller_spec.rb
@@ -3,154 +3,134 @@ require 'spec_helper'
 describe Accounts::ProjectsController do
   before { sign_up_and_mock_account }
 
+  let(:current_user){ @current_user }
+  let(:account){ mock_account }
+  let(:project){ mock_project }
+  let(:projects){ [project] }
+  let(:section){ mock_section }
+  let(:sections){ [section] }
+
   describe "with admin user" do
     before do
-      mock_account.should_receive(:admin?).with(@current_user).and_return true
-      mock_account.stub(:projects).and_return @projects = [mock_project]
+      account.should_receive(:admin?).with(current_user).and_return true
+      account.stub :projects => projects
+
+      projects.stub :find => project
     end
 
-    describe "on collection action" do
-      describe "GET index" do
-        before do
-          @projects.should_receive(:completed).and_return([@completed = mock(Project)])
-          @projects.should_receive(:active).and_return([@active = mock(Project)])
+    describe "GET index" do
+      before { projects.stub :completed => "completed projects" }
+      before { projects.stub :active => "active projects" }
+      before { get :index, :account_id => "1" }
 
-          get :index, :account_id => "1"
-        end
+      it { should assign_to(:projects).with("active projects") }
+      it { should assign_to(:completed).with("completed projects") }
+      it { should render_template(:index) }
+    end
 
-        it { should assign_to(:projects).with([@active]) }
-        it { should assign_to(:completed).with([@completed]) }
-        it { should render_template("index") }
+    describe "GET new" do
+      before { projects.stub :build => project }
+      before { get :new, :account_id => "1" }
+
+      it { should assign_to(:project).with(project) }
+      it { should render_template(:new) }
+    end
+
+    describe "POST create" do
+      before { projects.should_receive(:build).with({'these' => 'params'}).and_return project }
+
+      describe "with valid params" do
+        before { project.stub :save => true }
+        before { post :create, :account_id => "1", :project => {:these => 'params'} }
+
+        it { should assign_to(:project).with(project) }
+        it { should redirect_to(tasks_project_sections_url(project)) }
       end
 
-      describe "GET new" do
-        before do
-          @projects.should_receive(:build).and_return mock_project
+      describe "with invalid params" do
+        before { project.stub :save => false }
+        before { post :create, :account_id => "1", :project => {:these => 'params'} }
 
-          get :new, :account_id => "1"
-        end
-
-        it { should assign_to(:project).with(mock_project) }
-        it { should render_template("new") }
-      end
-
-      describe "POST create" do
-        before do
-          @projects.should_receive(:build).with({'these' => 'params'}).and_return mock_project
-        end
-
-        describe "with valid params" do
-          before do
-            mock_project.stub(:save).and_return true
-
-            post :create, :account_id => "1", :project => {:these => 'params'}
-          end
-
-          it { should assign_to(:project).with(mock_project) }
-          it { should redirect_to(tasks_project_sections_url(mock_project)) }
-        end
-
-        describe "with invalid params" do
-          before do
-            mock_project.stub(:save).and_return false
-
-            post :create, :account_id => "1", :project => {:these => 'params'}
-          end
-
-          it { should assign_to(:project).with(mock_project) }
-          it { should render_template("new") }
-        end
+        it { should assign_to(:project).with(project) }
+        it { should render_template(:new) }
       end
     end
 
-    describe "on member action" do
-      before do
-        @projects.should_receive(:find).with("2").and_return mock_project
+    describe "GET show" do
+      before { project.stub_chain :sections, :order => [section] }
+      before { get :show, :account_id => "1", :id => "2" }
+
+      it { should assign_to(:sections).with(sections) }
+      it { should assign_to(:project).with(project) }
+      it { should render_template(:show) }
+    end
+
+    describe "GET edit" do
+      before { get :edit, :account_id => "1", :id => "2" }
+
+      it { should assign_to(:project).with(project) }
+      it { should render_template(:edit) }
+    end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        before { project.should_receive(:update_attributes).with({'these' => 'params'}).and_return true }
+        before { put :update, :account_id => "1", :id => "2", :project => {:these => 'params'} }
+
+        it { should assign_to(:project).with(project) }
+        it { should set_the_flash }
+        it { should redirect_to(account_project_url(account, project)) }
       end
 
-      describe "GET show" do
-        before do
-          mock_project.stub_chain :sections, :order => [mock_section]
+      describe "with invalid params" do
+        before { project.should_receive(:update_attributes).with({'these' => 'params'}).and_return false }
+        before { put :update, :account_id => "1", :id => "2", :project => {:these => 'params'} }
 
-          get :show, :account_id => "1", :id => "2"
-        end
-
-        it { should assign_to(:sections).with([mock_section]) }
-        it { should assign_to(:project).with(mock_project) }
-        it { should render_template("show") }
-      end
-
-      describe "GET edit" do
-        before { get :edit, :account_id => "1", :id => "2" }
-
-        it { should assign_to(:project).with(mock_project) }
+        it { should assign_to(:project).with(project) }
         it { should render_template("edit") }
       end
+    end
 
-      describe "PUT update" do
-        describe "with valid params" do
-          before do
-            mock_project.should_receive(:update_attributes).with({'these' => 'params'}).and_return true
-            put :update, :account_id => "1", :id => "2", :project => {:these => 'params'}
-          end
+    describe "DELETE destroy" do
+      before { project.should_receive(:destroy) }
+      before { delete :destroy, :account_id => "1", :id => "2" }
 
-          it { should assign_to(:project).with(mock_project) }
-          it { should set_the_flash }
-          it { should redirect_to(account_project_url(mock_account, mock_project)) }
-        end
+      it { should redirect_to(account_projects_url(account)) }
+    end
 
-        describe "with invalid params" do
-          before do
-            mock_project.should_receive(:update_attributes).with({'these' => 'params'}).and_return false
-            put :update, :account_id => "1", :id => "2", :project => {:these => 'params'}
-          end
-
-          it { should assign_to(:project).with(mock_project) }
-          it { should render_template("edit") }
-        end
+    describe "PUT complete" do
+      before do
+        project.stub :completed=
+        project.stub :save
       end
 
-      describe "DELETE destroy" do
-        before do
-          mock_project.should_receive(:destroy)
-          delete :destroy, :account_id => "1", :id => "2"
-        end
+      it "should set completed flag to project to true" do
+        project.should_receive(:completed=).with(true)
+        project.should_receive(:save)
 
-        it { should redirect_to(account_projects_url(mock_account)) }
+        put :complete, :account_id => "1", :id => "2", :complete => "foo"
       end
 
-      describe "PUT complete" do
-        before do
-          mock_project.stub(:completed=)
-          mock_project.stub(:save)
-        end
+      it "should set completed flag to project to false" do
+        project.should_receive(:completed=).with(false)
+        project.should_receive(:save)
 
-        it "should set completed flag to project to true" do
-          mock_project.should_receive(:completed=).with(true)
-          mock_project.should_receive(:save)
+        put :complete, :account_id => "1", :id => "2"
+      end
 
-          put :complete, :account_id => "1", :id => "2", :complete => "foo"
-        end
+      it "redirects back to the projecs pages" do
+        project.stub(:completed=)
 
-        it "should set completed flag to project to false" do
-          mock_project.should_receive(:completed=).with(false)
-          mock_project.should_receive(:save)
+        put :complete, :account_id => "1", :id => "2", :complete => "foo"
 
-          put :complete, :account_id => "1", :id => "2"
-        end
-
-        it do
-          put :complete, :account_id => "1", :id => "2", :complete => "foo"
-
-          should redirect_to(account_project_url(mock_account, mock_project))
-        end
+        should redirect_to(account_project_url(account, project))
       end
     end
   end
 
   describe "with normal user" do
     before do
-      mock_account.should_receive(:admin?).with(@current_user).and_return false
+      account.should_receive(:admin?).with(current_user).and_return false
 
       ensure_deny_access_is_called
     end
