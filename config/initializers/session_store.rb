92a418f58bb9fa8e96bd4f104b8f261df41d9866
Radoslav Stankov <rstankov@gmail.com>
2011-06-05 17:11:12 +0300

    Compass is now a Railtie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Taskar::Application.config.session_store :cookie_store, :key => '_foo_session'
+Taskar::Application.config.session_store :cookie_store, :key => Application.app_cookie_key
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
