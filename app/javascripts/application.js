46a01639a218d12c8e5ba02eba144a04c7840000
Radoslav Stankov <rstankov@gmail.com>
2012-01-09 02:33:06 +0200

    Move behaviors.js into application.js

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -6,7 +6,21 @@
 //= require "extensions"
 //= require "rails"
 //= require "taskar"
-//= require "behaviors"
+
+CD3.Behaviors({
+  '#sections':            Taskar.Sections.SectionsList.initialize,
+  '#section_aside':       Taskar.Sections.Aside.initialize,
+  '#user_card':           Taskar.Sections.Aside.newStatusForm,
+  '#tasks':               Taskar.Sections.TaskList.initialize,
+  '#tasks_index':         Taskar.Sections.TaskList.behaviors,
+  '#sections_show':       Taskar.Sections.TaskList.behaviors,
+  '#tasks_show':          Taskar.Sections.Comments.behaviors,
+  '#feedback':            Taskar.Sections.Feedback,
+  '#live_search':         Taskar.UI.LiveSearch.Form,
+  'li.tasks_stats ul':    Taskar.Graphics.createPieChart
+});
+
+CD3.Behaviors(Taskar.Sections.Global.behaviors);
 
 Taskar.UI.StateCheckboxObserver();
 Taskar.Dnd.SortableObserver();
