17fa1e54066327b82d1a29cbcdf9398ec5df89d3
Radoslav Stankov <rstankov@gmail.com>
2012-02-12 23:35:07 +0200

    Remove obsolete account methods

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,17 +11,4 @@ class Account < ActiveRecord::Base
   has_many :account_users, :dependent => :destroy
   has_many :projects, :dependent => :destroy
   has_many :invitations, :dependent => :destroy
-
-  def owner?(user)
-    owner_id == user.id
-  end
-
-  def admin?(user)
-    if owner? user
-      true
-    else
-      account_user = account_users.find_by_user_id(user.id)
-      account_user && account_user.admin
-    end
-  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,49 +5,10 @@ describe Account do
 
   it { should belong_to(:owner) }
   it { should validate_presence_of(:name) }
-  it { Factory(:account).should validate_uniqueness_of(:name) }
+  it { create(:account).should validate_uniqueness_of(:name) }
 
   it { should have_many(:account_users) }
   it { should have_many(:users) }
   it { should have_many(:projects) }
   it { should have_many(:invitations) }
-
-  let(:account) { Factory(:account) }
-  let(:user) { Factory(:user) }
-
-  it "can tell if an user is his owner" do
-    account = create :account
-    account.owner?(account.owner).should be_true
-    account.owner?(create(:user)).should be_false
-  end
-
-  describe "#admin?" do
-    let(:account_user) do
-      AccountUser.new.tap do |account_user|
-        account_user.user    = user
-        account_user.account = account
-      end
-    end
-
-    it "returns false if account_user.admin is false" do
-      account_user.update_attribute :admin, false
-
-      account.admin?(user).should be_false
-    end
-
-    it "returns true if account_user.admin is true" do
-      account_user.update_attribute :admin, true
-
-      account.admin?(user).should be_true
-    end
-
-    it "returns false if account_user doesn't exists at all" do
-      account.admin?(user).should be_false
-    end
-
-    it "returns true if owner is same as user, independent of account_user" do
-      account.owner_id = user.id
-      account.admin?(user).should be_true
-    end
-  end
 end
